342e5ef918248e75bb8e9cae32c3a158ec370e3a
Natalia Correal <natalia.correal@uc.cl>
2024-06-26 23:27:11 -0400

    feat: add db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import db from "@astrojs/db";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), db()]
 });
-
